0200bdd8f7fd6e2510dfae9045037672e4157685
eotter-beep <krw122081@gmail.com>
2025-10-19 01:36:59 +0000

    Add venv creator

---
+++ venv.py
@@ -0,0 +1,6 @@
+import os
+os.system("python3 -m venv venv")
+os.system("pip install audio2vec")
+os.system("pip install quantumaudio")
+os.system("pip install numpy")
+os.system("pip install anyio")
